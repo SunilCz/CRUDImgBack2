07e92133d64333217192cb7d1e60932e22544f52
SunilCz <86451565+SunilCz@users.noreply.github.com>
2023-12-27 12:25:50 +0545

    Update blog.js

--- routes/blog.js
+++ routes/blog.js
@@ -45,17 +45,19 @@ const response = (res, status, result) => {
   res.status(status).json(result);
 };
 
-
-
-
 BlogRouter.get("/", async (req, res) => {
   const { page = 1, limit = 3 } = req.query;
 
   try {
+    const currentPage = parseInt(page);
     const blogs = await Blog.find()
       .sort("-createdOn")
-      .skip((page - 1) * limit)
-      .limit(limit);
+      .skip((currentPage - 1) * limit)
+      .limit(parseInt(limit));
+
+    const totalBlogs = await Blog.countDocuments();
+
+    const totalPages = Math.ceil(totalBlogs / limit);
 
     const blogsWithImages = blogs.map((blog) => ({
       _id: blog._id,
@@ -65,7 +67,7 @@ BlogRouter.get("/", async (req, res) => {
       createdOn: blog.createdOn,
     }));
 
-    response(res, 200, blogsWithImages);
+    response(res, 200, { blogs: blogsWithImages, totalPages });
   } catch (err) {
     console.log(err);
     response(res, 400, { error: err });
@@ -79,7 +81,6 @@ BlogRouter.get("/post/:id", async (req, res) => {
 
   try {
     if (mongoose.isValidObjectId(id)) {
-      // If it's a valid ObjectId, fetch the blog post by ID
       const blog = await Blog.findById(id);
 
       if (!blog) {
@@ -96,7 +97,6 @@ BlogRouter.get("/post/:id", async (req, res) => {
 
       response(res, 200, blogWithImage);
     } else {
-      // If it's not a valid ObjectId, treat it as a search query
       const results = await Blog.find({
         title: { $regex: new RegExp(id, "i") },
       });
@@ -108,8 +108,6 @@ BlogRouter.get("/post/:id", async (req, res) => {
     response(res, 500, { error: "Internal Server Error" });
   }
 });
-
-
 
 BlogRouter.post("/create", upload.single("image"), async (req, res) => {
   try {
@@ -159,12 +157,10 @@ BlogRouter.put("/update/:id", upload.single("image"), async (req, res) => {
       return response(res, 404, { error: "blog not found" });
     }
 
-    // Update fields
     blog.title = title;
     blog.content = content;
 
     if (newImage) {
-      // Delete the old image if it exists
       if (blog.image) {
         fs.unlinkSync(`uploads/${blog.image}`);
       }
@@ -180,9 +176,9 @@ BlogRouter.put("/update/:id", upload.single("image"), async (req, res) => {
 });
 
 BlogRouter.get("/search", async (req, res) => {
-  const { query } = req.query;
-
   try {
+    const { q: query } = req.query;
+
     if (!query) {
       return response(res, 400, { error: "Query parameter is required for search" });
     }
@@ -191,12 +187,11 @@ BlogRouter.get("/search", async (req, res) => {
       title: { $regex: new RegExp(query, "i") },
     });
 
-    response(res, 200, results);
+    response(res, 200, { query, results });
   } catch (error) {
-    response(res, 400, { error: error });
+    response(res, 400, { error: error.message });
   }
 });
-
 
 
 export default BlogRouter;
